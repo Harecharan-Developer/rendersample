47c535869a88f4c5f5205010105422e0e451fa5b
Hare_The_Coder <120263547+Harecharan-Developer@users.noreply.github.com>
2024-02-11 23:53:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import process
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+# Define a route
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.post("/")
+async def read_community_fisherman_data():
+    return process.read_community_fisherman_data()
+
